c9c32a108c3ca1ec8fb408ab2401126593828c08
xiaoqiping <727624698@qq.com>
2020-07-23 10:26:43 +0800

    no message

--- www/acquisition.szlcsc.com/demo_小米商品种类爬取.py
+++ www/acquisition.szlcsc.com/demo_小米商品种类爬取.py
@@ -6,29 +6,29 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def data_category():
-    url = 'https://www.vmall.com/'
-    r = requests.get(url)
-    bs = BeautifulSoup(r.text, "html.parser")
-    category_list = bs.find(class_="category-list").find_all(id='zxnav_8')
+    f = open('file/小米商品种类/aa.html', 'rb')  # 里面为文件路径
+
+    bs = BeautifulSoup(f.read(), "html.parser")
+    category_list = bs.find_all(class_='categoryHeader-G-sic')
+
+    boardsWrap_uKvGG = bs.find_all(class_='boardsWrap-uKvGG')
 
     result_data = []
-    for i in category_list:
-        for ii in i.find_all('li'):
-            print(ii.find('a').text.strip())
-            list_id = ii.find('a').get('href').split('-')[1]
-            url = 'https://openapi.vmall.com/mcp/queryPrd?lang=zh-CN&country=CN&portal=1&keyword='+str(list_id)+'&pageSize=20&pageNum=1&searchSortField=0&searchSortType=desc&searchFlag=1'
-            res = requests.get(url)
-            res = json.loads(res.text)
-            print(res['totalCount'])
-            if(  ii.find('a').text.strip() != '查看全部' ):
-                formated_data = {
-                    '种类': ii.find('a').text.strip(),
-                    '数目': res['totalCount'],
-                }
-                result_data.append(formated_data)
+    p = 0
+    for ii in category_list:
+        print(ii.find('span').text.strip())
 
-                df = pd.DataFrame(result_data)
-                df.to_csv(r"file/华为商品种类/华为生态商品种类.csv" , encoding='utf-8-sig')
+        totalCount = len(boardsWrap_uKvGG[p].find_all(class_='boardCard-3nuYn'))
+        formated_data = {
+            '圈子': ii.find('span').text.strip(),
+            '数目': totalCount,
+        }
+
+
+        result_data.append(formated_data)
+        df = pd.DataFrame(result_data)
+        df.to_csv(r"file/小米商品种类/小米社区圈子及数量.csv" , encoding='utf-8-sig')
+        p+=1
 if __name__ == '__main__':
     # 判断商品是否存在
     # def is_in_szlcsc_goods_sale_arr(arr, key):
--- www/damo/demo.py
+++ www/damo/demo.py
@@ -1,19 +1,55 @@
 #!/usr/bin/python3.8
 # -*- coding: UTF-8 -*-
-path = r'D:\config.json'
 
-file=open(r'D:\new_config.json','a', encoding='utf_8')
+import requests,json
+def prox():
 
-with open(path, 'r', encoding='utf-8') as f:
-    for line in f:
-        value = line[:-1]  # 去掉换行符
+    a={"features":{"isAllowCompanyMap":"WP_WIDGET_VAS_COMPANY_MAP"},"address":"中国 广东 广州市越秀区惠福西路294号首层商场内第5A号档","showAlert":True,"showTitle":True,"showResult":True,"companyName":"广州市越秀区民娜涛电子经营部"}
+    print(eval(a))
+    exit()
+    s = requests.session()
+    s.keep_alive = False
+    url = 'https://suying138.1688.com/page/contactinfo.htm?spm=a261y.7663282.autotrace-topNav.8.228f89bb4Tyl0L'
+    proxies = {'http': 'http://125.66.51.42:49936',
+               'https': 'https://125.66.51.42:49936'}
+    headers = {
 
-        value = value.replace(" ", "")
-        print(value)
-        file.write(value)
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
 
-file.close()
+    }
 
+    html = requests.get(url, headers=headers,proxies=proxies,timeout=(6.05, 27.05))
+    print(html.text)
+    exit()
+
+
+    ip_list = [
+        'http://60.216.20.210:8001',
+        'http://120.192.75.82:808',
+        'http://124.205.155.153:9090',
+        'http://117.141.155.242:53281',
+        'http://114.98.24.221:4216',
+        'http://163.125.29.106:8118'
+    ]
+
+
+    for i in ip_list:
+        ip_list1={'http':i}
+        headers = {
+            'Host': 'liu13432460447.b2b.hc360.com',
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
+            'Referer': 'https://liu13432460447.b2b.hc360.com/'
+        }
+
+        html = requests.get(url,headers=headers,proxies=proxies,timeout=(6.05, 27.05)).text
+        print(html)
+        break
+        if '<html>' in html:
+            pass
+        else:
+            print (i)
+if __name__=='__main__':
+    prox()
 
 
 
